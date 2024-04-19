322460e35ccfc8d93881df23a9d22bd7b784f048
dseurotech <davide.salvador@eurotech.com>
2024-04-19 08:28:06 +0200

    Ingredient model
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

---
+++ courseProject/src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    constructor(public name: string, public amount: number) {
+        //putting public before the argument names above automatically creates internal values
+        // this.name = name;
+        // this.amount = amount;
+    }
+}
