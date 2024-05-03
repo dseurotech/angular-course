1444b570a9325dab255d83e37b03bf7ebc3fc40e
dseurotech <davide.salvador@eurotech.com>
2024-05-03 10:38:59 +0200

    adding environment to store secrets in local-only files
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

--- courseProject/src/app/data-storage.service.ts
+++ courseProject/src/app/data-storage.service.ts
@@ -3,30 +3,32 @@ import { Injectable } from '@angular/core';
 import { RecipeService } from './recipes/recipe.service';
 import { Recipe } from './recipes/recipe.model';
 import { map, tap } from 'rxjs';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipesService: RecipeService) { }
+  constructor(private http: HttpClient, private recipesService: RecipeService) {
+  }
 
   storeRecipies() {
     const recipes = this.recipesService.getRecipes();
-    this.http.put("https://angular-course-ba273-default-rtdb.europe-west1.firebasedatabase.app/recipes.json", recipes)
+    this.http.put(environment.firebaseDbUrl + "/recipes.json", recipes)
       .subscribe(response => { console.log(response) });
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>("https://angular-course-ba273-default-rtdb.europe-west1.firebasedatabase.app/recipes.json")
+    return this.http.get<Recipe[]>(environment.firebaseDbUrl + "/recipes.json")
       .pipe(map(recipes => {
         return recipes.map(recipe => {
           //initialize the ingredients if it returns as null from server
           return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
         });
       }),
-    tap(recipes=>{
-      this.recipesService.setRecipes(recipes);
-    }));
+        tap(recipes => {
+          this.recipesService.setRecipes(recipes);
+        }));
   }
 }
