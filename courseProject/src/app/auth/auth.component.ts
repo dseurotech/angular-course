01974b4facfcecad2ff6b08564bbe56f1367bd45
dseurotech <davide.salvador@eurotech.com>
2024-05-03 12:31:02 +0200

    signUp and signIn logic
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

--- courseProject/src/app/auth/auth.component.ts
+++ courseProject/src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -9,16 +11,41 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent {
 
   isLoginMode: boolean = true;
+  isLoading: boolean = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) { }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(authForm: NgForm) {
-
-
-
-    console.log(authForm.value);
-    authForm.reset();
+    if (!authForm.valid) {
+      return;
+    }
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+    this.isLoading = true;
+    this.error = null;
+    let httpCallOutcome: Observable<AuthResponseData>;
+    if (this.isLoginMode) {
+      httpCallOutcome = this.authService.login(email, password);
+    } else {
+      httpCallOutcome = this.authService.signUp(email, password);
+    }
+    
+    httpCallOutcome
+      .subscribe({
+        next: authData => {
+          this.isLoading = false;
+          console.log(authData);
+        },
+        error: errorMessage => {
+          this.isLoading = false;
+          this.error = errorMessage;
+        }
+      });
+    // authForm.reset();
   }
 }
--- courseProject/src/app/auth/auth.service.ts
+++ courseProject/src/app/auth/auth.service.ts
@@ -1,17 +1,69 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './../../environments/environment';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
+export interface AuthResponseData {
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string,
+  registered?: boolean
+}
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor(private http: HttpClient) { }
   //sign up: https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
-//sign in: https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+  //sign in: https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
 
   signUp(username: string, password: string) {
-    this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp')
-
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp', {
+      'email': username,
+      'password': password,
+      'returnSecureToken': true
+    }, {
+      params: { key: environment.firebaseApiKey }
+    })
+      .pipe(catchError(errorRes => {
+        let errorMessage = "An unknown error occurred!";
+        if (!errorRes.error || !errorRes.error.error || !errorRes.error.error.message) {
+          return throwError(() => errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case "EMAIL_EXISTS":
+            errorMessage = "This email already exists!";
+        }
+        return throwError(() => errorMessage);
+      }));
+  }
+  login(username: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword', {
+      'email': username,
+      'password': password,
+      'returnSecureToken': true
+    }, {
+      params: { key: environment.firebaseApiKey }
+    })
+      .pipe(catchError(errorRes => {
+        let errorMessage = "An unknown error occurred!";
+        if (!errorRes.error || !errorRes.error.error || !errorRes.error.error.message) {
+          return throwError(() => errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case "INVALID_LOGIN_CREDENTIALS":
+            errorMessage = "Invalid login credentials";
+          case "EMAIL_NOT_FOUND":
+            errorMessage = "Unkown user";
+          case "INVALID_PASSWORD":
+            errorMessage = "Wrong password";
+          case "USER_DISABLED":
+            errorMessage = "User disabled";
+        }
+        return throwError(() => errorMessage);
+      }));
   }
 }
