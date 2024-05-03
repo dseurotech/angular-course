950f4ffaab0ec5f1ff3c0a7219c0ae0198a31988
dseurotech <davide.salvador@eurotech.com>
2024-05-03 09:32:35 +0200

    only fetch remote recipes when no recipes are present locally, to avoid losing updates on recipe edit
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

--- courseProject/src/app/recipes/recipes-resolver.service.ts
+++ courseProject/src/app/recipes/recipes-resolver.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import { DataStorageService } from '../data-storage.service';
 import { ActivatedRouteSnapshot, MaybeAsync, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<Recipe[]> {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipesService.getRecipes();
+    if (recipes.length === 0) {∏
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
-
 }
