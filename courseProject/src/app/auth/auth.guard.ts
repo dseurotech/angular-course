ba62214ff2544eff0b07e5fec756681ddc34d596
dseurotech <davide.salvador@eurotech.com>
2024-05-06 12:34:38 +0200

    avoiding subscription accumulation
    Signed-off-by: dseurotech <davide.salvador@eurotech.com>

--- courseProject/src/app/auth/auth.guard.ts
+++ courseProject/src/app/auth/auth.guard.ts
@@ -1,19 +1,21 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "./auth.service";
-import { map } from "rxjs";
+import { map, take } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private authSvc: AuthService, private router:Router) { }
+  constructor(private authSvc: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    return this.authSvc.user.pipe(map(usr => {
-      const userIsValid = !!usr?.token;
-      if(userIsValid){
-        return true;
-      }
-      return this,this.router.createUrlTree(["/auth"]);
-    }));
+    return this.authSvc.user.pipe(
+      take(1),
+      map(usr => {
+        const userIsValid = !!usr?.token;
+        if (userIsValid) {
+          return true;
+        }
+        return this, this.router.createUrlTree(["/auth"]);
+      }));
   }
 }
